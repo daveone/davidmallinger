166a5f36dd4cafac95ae609dc684c276490636bd
David Mallinger <contact@davidmallinger.de>
2015-01-20 12:22:41 +0100

    vers3

---
+++ WebEng2014/sencha/timer/app/view/Main.js
@@ -0,0 +1,33 @@
+Ext.define('Timer.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Welcome',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Timer - David Mallinger'
+                },
+
+                html: [
+                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
+                ].join("")
+            }
+        ]
+    }
+});
